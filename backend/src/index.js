f6d7a974bd0be6e66e12cead32ba2a7c5ba1deea
Elber Bastos de Oliveira <elber_gyn@hotmail.com>
2020-03-29 01:13:57 -0300

    frontend app

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,9 @@ const express = require("express");
 const routers = require("./router");
 
 const app = express();
-app.use(cors);
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routers);
